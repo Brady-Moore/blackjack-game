a1697f0bb00584dec2c043cae304156c6f87b14a
Bradoshado <bradoshado@gmail.com>
2024-11-12 21:43:17 +0900

    changed card suit aria-label to text

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,28 @@ const Card = ({ card }) => {
   const value = card.slice(0, -1);
   const suit = card.slice(-1);
   const isRed = suit === "♥" || suit === "♦";
+  let suitName = "";
+  switch (suit) {
+    case "♥":
+      suitName = "hearts";
+      break;
+    case "♦":
+      suitName = "diamonds";
+      break;
+    case "♠":
+      suitName = "spades";
+      break;
+    case "♣":
+      suitName = "clubs";
+      break;
+  }
 
   return (
-    <div className="card" role="img" aria-label={`Card: ${value} of ${suit}`}>
+    <div
+      className="card"
+      role="img"
+      aria-label={`Card: ${value} of ${suitName}`}
+    >
       <div className={`card-corner top-left ${isRed ? "red" : ""}`}>
         <span className="card-value">{value}</span>
         <span className="card-suit">{suit}</span>
